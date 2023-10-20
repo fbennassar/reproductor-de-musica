2ab2a2bced9f2a204441f84435361f5b18532f18
Francisco Bennassar <fbajo97@gmail.com>
2023-10-19 20:57:15 -0400

    CSS DONE

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ var jsonMusic = [
         "Title": "Not Long For This World",
         "Artist": "Slipknot",
         "Date": "2019",
-        "link": "http://127.0.0.1:5500/music/not_long_for_this_world.mp3"
+        "link": "http://127.0.0.1:5500/music/not_long_for_this_world.mp3",
+        "photo": "http://127.0.0.1:5500/img/slipknot.jpg"
     },
 
     {
         "Title": "The trooper",
         "Artist": "Iron Maiden",
         "Date": "1983",
-        "link": "http://127.0.0.1:5500/music/the_trooper.mp3"
+        "link": "http://127.0.0.1:5500/music/the_trooper.mp3",
+        "photo": "http://127.0.0.1:5500/img/iron_maiden.jpg"
     },
 
     {
         "Title": "Back in black",
         "Artist": "AC/DC",
         "Date": "1980",
-        "link": "http://127.0.0.1:5500/music/back_in_black.mp3"
+        "link": "http://127.0.0.1:5500/music/back_in_black.mp3",
+        "photo": "http://127.0.0.1:5500/img/ac_dc.jpg"
     }
 ]
 
@@ -29,7 +32,7 @@ function trackList() {
         cont ++;
         var track = document.createElement("div");
         track.classList.add("track");
-        track.innerHTML = cont + " " + jsonMusic[i].Title + " - " + jsonMusic[i].Artist + " " + jsonMusic[i].Date +
+        track.innerHTML = cont + " " + jsonMusic[i].Title + " - " + jsonMusic[i].Artist + " " + jsonMusic[i].Date + " " +
         "<button class='playButton' onclick='playMusic(" + JSON.stringify(jsonMusic[i]) +")'> Play</button>";
         trackList.appendChild(track);
 
@@ -37,6 +40,8 @@ function trackList() {
 }
 function playMusic(select) {
     var player = document.getElementById("audioPlayer");
+    var coverPhoto = document.getElementById("coverPhoto");
+    coverPhoto.src = select.photo;
     player.src = select.link;
     player.play();
     player.addEventListener("ended", function() {
@@ -60,6 +65,8 @@ function nextSong() {
     });
     var nextSongIndex = (currentSongIndex + 1) % jsonMusic.length;
     var nextSong = jsonMusic[nextSongIndex];
+    var coverPhoto = document.getElementById("coverPhoto");
+    coverPhoto.src = nextSong.photo;
     player.src = nextSong.link;
     player.play();
     var songName = document.getElementById("ti");
@@ -76,6 +83,8 @@ function previousSong() {
     });
     var previousSongIndex = (currentSongIndex - 1 + jsonMusic.length) % jsonMusic.length;
     var previousSong = jsonMusic[previousSongIndex];
+    var coverPhoto = document.getElementById("coverPhoto");
+    coverPhoto.src = previousSong.photo;
     player.src = previousSong.link;
     player.play();
     var songName = document.getElementById("ti");
@@ -89,7 +98,7 @@ var repeatMode = false;
 function repeatSong() {
     var repeatButton = document.getElementById("repeatButton");
     repeatMode = !repeatMode;
-    if (repeatMode == true) {
+    if (repeatMode === true) {
         repeatButton.classList.add("active");
     } else {
         repeatButton.classList.remove("active");
@@ -105,7 +114,10 @@ function commentBox() {
      var commentjs = document.getElementById("comment").value;
     var commentBoxjs = document.createElement("div");
     var namejs = document.getElementById("username").value;
-    commentBoxjs.innerHTML = namejs + ": " + commentjs;
+    commentBoxjs.innerHTML = namejs + ": " + commentjs + "<br>" + "<hr>";
     commentsContainer.appendChild(commentBoxjs);
+    var commentjs = document.getElementById("comment");
     commentjs.value = "";
+    var usernamejs = document.getElementById("username");
+    usernamejs.value = ""; 
 }
